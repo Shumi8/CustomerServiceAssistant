8ef1088e47d91c26eefb2bf522ebff87f80acdc8
Muhammad Shumyl Akbar <68348109+Shumi8@users.noreply.github.com>
2024-10-16 23:10:25 +0100

    Update driver_setup.py

--- driver_setup.py
+++ driver_setup.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 def get_chrome_driver():
     try:
         logging.info("Opening Chrome Driver")
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.198 Safari/537.36"
+        user_agent = ""
 
         chrome_options = Options()
         chrome_options.add_argument("--headless")
